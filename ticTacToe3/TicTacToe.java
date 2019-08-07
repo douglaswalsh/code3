72a0294469fc244037121060f26bc45701d39551
Douglas Walsh <douglaswalsh3@gmail.com>
2019-08-06 22:06:05 -0400

    new game loop

--- ticTacToe3/TicTacToe.java
+++ ticTacToe3/TicTacToe.java
@@ -1,46 +1,30 @@
 package ticTacToe3;
 
-
 public class TicTacToe {
 	public static void main(String[] args) {
-		int round = 0;
 		GameHandler game = new GameHandler();
-		Player pOne = new Player();
-		pOne.id = 1;
-		pOne.setMark();
-		Player pTwo = new Player();
-		pTwo.id = 2;
-		pTwo.setMark();
-		System.out.println("welcome to TICTACTOE, here's a board");
-		game.printBoard(game.board);
+		int round = 0;
+		System.out.println("TICTACTOE");
 
 		while (round < 8 && game.gameWon(game.board) == false) {
-			
-			if (round == 0 || round == 2 || round == 4 || round == 6 || round == 8) {
-				System.out.println("Player 1, make your move by entering row coordinate and then column");
-				game.setMove();
-				if (game.validateMove(game.getMove()) == true) {
-					game.makeMove(game.getMove(), pOne.getMark());
-					round++;
-					game.printBoard(game.board);
-					//System.out.println("is the game over? " + game.gameWon(game.board));
+			char mark;
+			System.out.println("make your move by entering row coordinate and then column");
+			game.setMove();
+			if (game.validateMove(game.getMove()) == true) {
+				if (round % 2 == 0) {
+					mark = 'x';
 				} else {
-					System.out.println("illegal move, pls try again");
+					mark = 'o';
 				}
+				game.makeMove(game.getMove(), mark);
+				round++;
 			} else {
-				System.out.println("Player 2, make your move");
-				game.setMove();
-				if (game.validateMove(game.getMove()) == true) {
-					game.makeMove(game.getMove(), pTwo.getMark());
-					round++;
-					game.printBoard(game.board);
-					//System.out.println("is the game over? " + game.gameWon(game.board));
-				} else {
-					System.out.println("illegal move, pls try again");
-				}
+				System.out.println("illegal move, pls try again");
 			}
+			game.printBoard(game.board);
 		}
-		if (game.gameWon(game.board) == true){
+
+		if (game.gameWon(game.board) == true) {
 			System.out.println("game over");
 		}
 	}
